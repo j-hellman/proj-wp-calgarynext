64483d187dcbb0a395ed8354b34491ab4f72050c
Jaime Hellman <jaime_hellman@hotmail.com>
2021-06-30 20:31:55 -0300

    fix entertainment page links

--- wp-content/themes/FoundationPress/category/category-sports-entertainment.php
+++ wp-content/themes/FoundationPress/category/category-sports-entertainment.php
@@ -18,7 +18,7 @@ get_header(); ?>
     <h2>TOP STORY</h2>
   </div>
   <div class="topstory-heading topstory-entertainment link-hover"> <!-- topstory-entertainment set the background image -->
-    <h3><a href="http://localhost/calgary_next/sports-entertainment/lebron-back-to-miami-again/">LeBron Back to Miami, AGAIN!</a></h3>
+    <h3><a href="http://localhost/proj-wp-calgarynext/sports-entertainment/lebron-back-to-miami-again/">LeBron Back to Miami, AGAIN!</a></h3>
     <p>May 6, 2021</p>
   </div>
   <hr>
@@ -29,7 +29,7 @@ get_header(); ?>
   <!-- Card 1 -->
   <div class="card-container">
     <div class="card-image">
-      <img src="../../calgary_next\wp-content\themes\FoundationPress\src\assets\images\topic-entertainment\stampede.jpg" alt="Stampede image">
+      <img src="../../proj-wp-calgarynext\wp-content\themes\FoundationPress\src\assets\images\topic-entertainment\stampede.jpg" alt="Stampede image">
     </div>
     <div class="card-wrap">
       <div class="card-title">
@@ -42,7 +42,7 @@ get_header(); ?>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra justo euismod diam nunc, ut condimentum commodo. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
       </div>
       <div>
-        <button class="readMore-button" onclick="window.location.href='http:localhost/calgary_next/sports-entertainment/stampede-set-to-resume-2021/'">READ MORE</button>
+        <button class="readMore-button" onclick="window.location.href='http:localhost/proj-wp-calgarynext/sports-entertainment/stampede-set-to-resume-2021/'">READ MORE</button>
       </div>
     </div>
   </div>
@@ -50,7 +50,7 @@ get_header(); ?>
   <!-- Card 2 -->
   <div class="card-container">
     <div class="card-image">
-      <img src="../../calgary_next\wp-content\themes\FoundationPress\src\assets\images\topic-entertainment\hockeyWoman.jpg" alt="Hockey woman image">
+      <img src="../../proj-wp-calgarynext\wp-content\themes\FoundationPress\src\assets\images\topic-entertainment\hockeyWoman.jpg" alt="Hockey woman image">
     </div>
     <div class="card-wrap">
       <div class="card-title">
@@ -63,7 +63,7 @@ get_header(); ?>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra justo euismod diam nunc, ut condimentum commodo.</p>
       </div>
       <div>
-        <button class="readMore-button" onclick="window.location.href='http:localhost/calgary_next/sports-entertainment/womens-world-hockey-championship-in-calgary/'">READ MORE</button>
+        <button class="readMore-button" onclick="window.location.href='http:localhost/proj-wp-calgarynext/sports-entertainment/womens-world-hockey-championship-in-calgary/'">READ MORE</button>
       </div>
     </div>
   </div>
@@ -71,7 +71,7 @@ get_header(); ?>
   <!-- Card 3 -->
   <div class="card-container">
     <div class="card-image">
-      <img src="../../calgary_next\wp-content\themes\FoundationPress\src\assets\images\topic-entertainment\music.jpg" alt="Music Convention image">
+      <img src="../../proj-wp-calgarynext\wp-content\themes\FoundationPress\src\assets\images\topic-entertainment\music.jpg" alt="Music Convention image">
     </div>
     <div class="card-wrap">
       <div class="card-title">
@@ -84,7 +84,7 @@ get_header(); ?>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra justo euismod diam nunc, ut condimentum commodo. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
       </div>
       <div>
-        <button class="readMore-button" onclick="window.location.href='http:localhost/calgary_next/sports-entertainment/music-cities-convention-coming-to-calgary-next-semester/'">READ MORE</button>
+        <button class="readMore-button" onclick="window.location.href='http:localhost/proj-wp-calgarynext/sports-entertainment/music-cities-convention-coming-to-calgary-next-semester/'">READ MORE</button>
       </div>
     </div>
   </div>
@@ -92,7 +92,7 @@ get_header(); ?>
   <!-- Card 4 -->
   <div class="card-container">
     <div class="card-image">
-      <img src="../../calgary_next\wp-content\themes\FoundationPress\src\assets\images\topic-entertainment\outside.jpg" alt="Safe Outside image">
+      <img src="../../proj-wp-calgarynext\wp-content\themes\FoundationPress\src\assets\images\topic-entertainment\outside.jpg" alt="Safe Outside image">
     </div>
     <div class="card-wrap">
       <div class="card-title">
@@ -105,7 +105,7 @@ get_header(); ?>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra justo euismod diam nunc, ut condimentum commodo.</p>
       </div>
       <div>
-        <button class="readMore-button" onclick="window.location.href='http:localhost/calgary_next/sports-entertainment/how-to-stay-safe-outside-during-covid-19-pandamic/'">READ MORE</button>
+        <button class="readMore-button" onclick="window.location.href='http:localhost/proj-wp-calgarynext/sports-entertainment/how-to-stay-safe-outside-during-covid-19-pandamic/'">READ MORE</button>
       </div>
     </div>
   </div>
@@ -113,7 +113,7 @@ get_header(); ?>
   <!-- Card 5 -->
   <div class="card-container">
     <div class="card-image">
-      <img src="../../calgary_next\wp-content\themes\FoundationPress\src\assets\images\topic-entertainment\calgary.jpg" alt="Calgary image">
+      <img src="../../proj-wp-calgarynext\wp-content\themes\FoundationPress\src\assets\images\topic-entertainment\calgary.jpg" alt="Calgary image">
     </div>
     <div class="card-wrap">
       <div class="card-title">
@@ -126,7 +126,7 @@ get_header(); ?>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra justo euismod diam nunc, ut condimentum commodo. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
       </div>
       <div>
-        <button class="readMore-button" onclick="window.location.href='http:localhost/calgary_next/sports-entertainment/enjoy-the-may-long-weekend-doing-different-things-in-calgary/'">READ MORE</button>
+        <button class="readMore-button" onclick="window.location.href='http:localhost/proj-wp-calgarynext/sports-entertainment/enjoy-the-may-long-weekend-doing-different-things-in-calgary/'">READ MORE</button>
       </div>
     </div>
   </div>
