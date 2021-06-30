05b4ba651b4d2c5ba375205568a3e8be0af30e5b
Jaime Hellman <jaime_hellman@hotmail.com>
2021-06-30 20:52:39 -0300

    clean code single.php

--- wp-content/themes/FoundationPress/single.php
+++ wp-content/themes/FoundationPress/single.php
@@ -14,12 +14,12 @@ get_header(); ?>
 		<main class="main-content">
 			<?php while ( have_posts() ) : the_post(); ?>
 				<?php get_template_part( 'template-parts/content', '' ); ?>
-				<!-- <?php the_post_navigation(); ?> -->
-				<!-- <?php comments_template(); ?> -->
+				<?php // the_post_navigation(); ?>
+				<?php // comments_template(); ?>
 
 			<?php endwhile; ?>
 		</main>
-		<!-- <?php get_sidebar(); ?> -->
+		<?php // get_sidebar(); ?>
 	</div>
 </div>
 <?php get_footer();
