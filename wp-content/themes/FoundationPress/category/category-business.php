a3948cd9f103167002f8696dbf102f5330e8838c
Jaime Hellman <jaime_hellman@hotmail.com>
2021-06-29 21:50:03 -0300

    fix business page links

--- wp-content/themes/FoundationPress/category/category-business.php
+++ wp-content/themes/FoundationPress/category/category-business.php
@@ -18,7 +18,7 @@ get_header(); ?>
     <h2>TOP STORY</h2>
   </div>
   <div class="topstory-heading topstory-business link-hover"> <!-- topstory-business set the background image -->
-    <h3><a href="http://localhost/calgary_next/business/the-impending-market-crash/">The Impending Market Crash</a></h3>
+    <h3><a href="http://localhost/proj-wp-calgarynext/business/the-impending-market-crash/">The Impending Market Crash</a></h3>
     <p>May 6, 2021</p>
   </div>
   <hr>
@@ -29,7 +29,7 @@ get_header(); ?>
   <!-- Card 1 -->
   <div class="card-container">
     <div class="card-image">
-      <img src="../../calgary_next\wp-content\themes\FoundationPress\src\assets\images\topic-business\bitcoin.jpg" alt="Bitcoin image">
+      <img src="../../proj-wp-calgarynext\wp-content\themes\FoundationPress\src\assets\images\topic-business\bitcoin.jpg" alt="Bitcoin image">
     </div>
     <div class="card-wrap">
       <div class="card-title">
@@ -42,7 +42,7 @@ get_header(); ?>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra justo euismod diam nunc, ut condimentum commodo. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
       </div>
       <div>
-        <button class="readMore-button" onclick="window.location.href='http:localhost/calgary_next/business/best-time-to-buy-crypto/'">READ MORE</button>
+        <button class="readMore-button" onclick="window.location.href='http:localhost/proj-wp-calgarynext/business/best-time-to-buy-crypto/'">READ MORE</button>
       </div>
     </div>
   </div>
@@ -50,7 +50,7 @@ get_header(); ?>
   <!-- Card 2 -->
   <div class="card-container">
     <div class="card-image">
-      <img src="../../calgary_next\wp-content\themes\FoundationPress\src\assets\images\topic-business\laptop.jpg" alt="Laptop image">
+      <img src="../../proj-wp-calgarynext\wp-content\themes\FoundationPress\src\assets\images\topic-business\laptop.jpg" alt="Laptop image">
     </div>
     <div class="card-wrap">
       <div class="card-title">
@@ -63,7 +63,7 @@ get_header(); ?>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra justo euismod diam nunc, ut condimentum commodo.</p>
       </div>
       <div>
-        <button class="readMore-button" onclick="window.location.href='http:localhost/calgary_next/business/alberta-now-home-to-3000-tech-companies/'">READ MORE</button>
+        <button class="readMore-button" onclick="window.location.href='http:localhost/proj-wp-calgarynext/business/alberta-now-home-to-3000-tech-companies/'">READ MORE</button>
       </div>
     </div>
   </div>
@@ -71,7 +71,7 @@ get_header(); ?>
   <!-- Card 3 -->
   <div class="card-container">
     <div class="card-image">
-      <img src="../../calgary_next\wp-content\themes\FoundationPress\src\assets\images\topic-business\house.jpg" alt="House image">
+      <img src="../../proj-wp-calgarynext\wp-content\themes\FoundationPress\src\assets\images\topic-business\house.jpg" alt="House image">
     </div>
     <div class="card-wrap">
       <div class="card-title">
@@ -84,7 +84,7 @@ get_header(); ?>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra justo euismod diam nunc, ut condimentum commodo. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
       </div>
       <div>
-        <button class="readMore-button" onclick="window.location.href='http:localhost/calgary_next/business/calgary-housing-market-sees-best-sales-in-over-two-decades/'">READ MORE</button>
+        <button class="readMore-button" onclick="window.location.href='http:localhost/proj-wp-calgarynext/business/calgary-housing-market-sees-best-sales-in-over-two-decades/'">READ MORE</button>
       </div>
     </div>
   </div>
@@ -92,7 +92,7 @@ get_header(); ?>
   <!-- Card 4 -->
   <div class="card-container">
     <div class="card-image">
-      <img src="../../calgary_next\wp-content\themes\FoundationPress\src\assets\images\topic-business\openshop.jpg" alt="Openshop image">
+      <img src="../../proj-wp-calgarynext\wp-content\themes\FoundationPress\src\assets\images\topic-business\openshop.jpg" alt="Openshop image">
     </div>
     <div class="card-wrap">
       <div class="card-title">
@@ -105,7 +105,7 @@ get_header(); ?>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra justo euismod diam nunc, ut condimentum commodo.</p>
       </div>
       <div>
-        <button class="readMore-button" onclick="window.location.href='http:localhost/calgary_next/business/small-businesses-urge-calgarians-to-shop-more/'">READ MORE</button>
+        <button class="readMore-button" onclick="window.location.href='http:localhost/proj-wp-calgarynext/business/small-businesses-urge-calgarians-to-shop-more/'">READ MORE</button>
       </div>
     </div>
   </div>
@@ -113,7 +113,7 @@ get_header(); ?>
   <!-- Card 5 -->
   <div class="card-container">
     <div class="card-image">
-      <img src="../../calgary_next\wp-content\themes\FoundationPress\src\assets\images\topic-business\amazon.jpg" alt="Amazon image">
+      <img src="../../proj-wp-calgarynext\wp-content\themes\FoundationPress\src\assets\images\topic-business\amazon.jpg" alt="Amazon image">
     </div>
     <div class="card-wrap">
       <div class="card-title">
@@ -126,7 +126,7 @@ get_header(); ?>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra justo euismod diam nunc, ut condimentum commodo. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
       </div>
       <div>
-        <button class="readMore-button" onclick="window.location.href='http:localhost/calgary_next/business/amazon-postpones-prime-day/'">READ MORE</button>
+        <button class="readMore-button" onclick="window.location.href='http:localhost/proj-wp-calgarynext/business/amazon-postpones-prime-day/'">READ MORE</button>
       </div>
     </div>
   </div>
